fdffb1b861701b208dccbac3a079a27998eefa48
Rory Shanks <megalan247@users.noreply.github.com>
2020-04-07 19:10:21 +0200

    Removed cap finder

--- cpp/iedriver/CommandHandlers/NewSessionCommandHandler.cpp
+++ cpp/iedriver/CommandHandlers/NewSessionCommandHandler.cpp
@@ -629,12 +629,6 @@ bool NewSessionCommandHandler::MatchCapabilities(const IECommandExecutor& execut
       return false;
     }
 
-    if (capability_name.find(":") != std::string::npos &&
-        (capability_name != IE_DRIVER_EXTENSIONS_CAPABILITY &&
-        capability_name.find("test:") == std::string::npos)) {
-      *error_message = capability_name + " is an unknown extension capability for IE";
-      return false;
-    }
   }
 
   return true;
